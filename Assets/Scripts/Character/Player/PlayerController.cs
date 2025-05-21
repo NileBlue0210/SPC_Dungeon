f6980c28157454ce2aae240a6b5a0a1cb5d2097a
DESKTOP-FVBU6NK\Sejin <sjb0710@gmail.com>
2025-05-21 17:04:31 +0900

    Feat: 플레이어 시야 관련 처리 추가
    플레이어 하위에 카메라 배치, Player Input Action에 시야와 관련된 Input요소인 Look 추가

--- Assets/Scripts/Character/Player/PlayerController.cs
+++ Assets/Scripts/Character/Player/PlayerController.cs
@@ -2,16 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// 플레이어 동작을 제어하는 스크립트
+/// </summary>
 public class PlayerController : MonoBehaviour
 {
+    [Header("플레이어 스테이터스")]
+    [SerializeField] private float moveSpeed;
+    private Vector2 curMoveInput;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SetPlayerStatus();
     }
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        
+    }
+
+    private void SetPlayerStatus()
+    {
+        moveSpeed = 5f;
+    }
+
+    private void Move()
     {
         
     }
